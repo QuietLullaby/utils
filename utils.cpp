162d5b6e6ffec37562294c5876ea2fe6b02e161f
jeremy_cumin <j5cumin@enib.fr>
2018-06-19 12:25:58 +0200

    Small changes

--- utils.cpp
+++ utils.cpp
@@ -9,25 +9,25 @@ int main(int argc,char ** argv)
     for (int i=1;i<argc;i++){
       cout << "argument detected : " << argv[i] << endl;
       if (string(argv[i])=="-u"){
-        cout << "condition -u detected\n";
         system("sudo apt-get update && sudo apt-get upgrade");
       }
       else if (string(argv[i])=="-p"){
-        cout << "condition -p detected\n";
         system("sudo apt-get purge wolfram-engine bluej greenfoot nodered nuscratch scratch sonic-pi libreoffice claws-mail claws-mail-i18n minecraft-pi python-pygame");
       }
       else if (string(argv[i])=="-a"){
-        cout << "condition -a detected\n";
         system("sudo apt-get autoclean && sudo apt-get autoremove");
       }
-      else if (string(argv[i])=="-h"){
-        cout << "use :\n -u to update and upgrade\n -p to purge unnecessary package\n -a to autoclean and autoremove\n -all to do everything\n";
+      else if (string(argv[i])=="-r"){
+        system("sudo reboot");
       }
       else if (string(argv[i])=="-all"){
-        cout << "condition -all detected\n";
         system("sudo apt-get update && sudo apt-get upgrade");
         system("sudo apt-get purge wolfram-engine bluej greenfoot nodered nuscratch scratch sonic-pi libreoffice claws-mail claws-mail-i18n minecraft-pi python-pygame");
         system("sudo apt-get autoclean && sudo apt-get autoremove");
+        system("sudo reboot");
+      }
+      else if (string(argv[i])=="-h"){
+        cout << "use :\n -u to update and upgrade\n -p to purge unnecessary package\n -a to autoclean and autoremove\n -r to reboot\n -all to do everything\n";
       }
     }
   }
