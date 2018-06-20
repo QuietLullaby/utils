8a8b2ef463716ba801040540813ef6a5d3d922a8
jeremy_cumin <j5cumin@enib.fr>
2018-06-20 10:32:13 +0200

    Changes

--- utils.cpp
+++ utils.cpp
@@ -9,26 +9,32 @@ int main(int argc,char ** argv)
     for (int i=1;i<argc;i++){
       cout << "argument detected : " << argv[i] << endl;
       if (string(argv[i])=="-u"){
-        system("sudo apt-get update && sudo apt-get upgrade");
+        system("sudo apt-get -y update && sudo apt-get -y upgrade");
       }
       else if (string(argv[i])=="-p"){
-        system("sudo apt-get purge wolfram-engine bluej greenfoot nodered nuscratch scratch sonic-pi libreoffice claws-mail claws-mail-i18n minecraft-pi python-pygame");
+        system("sudo apt-get purge -y wolfram-engine bluej greenfoot nodered nuscratch scratch sonic-pi libreoffice claws-mail claws-mail-i18n minecraft-pi python-pygame");
       }
       else if (string(argv[i])=="-a"){
-        system("sudo apt-get autoclean && sudo apt-get autoremove");
+        system("sudo apt-get autoclean && sudo apt-get -y autoremove");
       }
       else if (string(argv[i])=="-r"){
         system("sudo reboot");
       }
+      else if (string(argv[i])=="-i"){
+        system("sudo apt-get install -y nautilus");
+      }
       else if (string(argv[i])=="-all"){
-        system("sudo apt-get update && sudo apt-get upgrade");
-        system("sudo apt-get purge wolfram-engine bluej greenfoot nodered nuscratch scratch sonic-pi libreoffice claws-mail claws-mail-i18n minecraft-pi python-pygame");
-        system("sudo apt-get autoclean && sudo apt-get autoremove");
+        system("sudo apt-get install -y nautilus");
+        system("sudo apt-get purge -y wolfram-engine bluej greenfoot nodered nuscratch scratch sonic-pi libreoffice claws-mail claws-mail-i18n minecraft-pi python-pygame");
+        system("sudo apt-get update -y && sudo apt-get upgrade -y");
+        system("sudo apt-get autoclean && sudo apt-get autoremove -y");
         system("sudo reboot");
       }
       else if (string(argv[i])=="-h"){
         cout << "use :\n -u to update and upgrade\n -p to purge unnecessary package\n -a to autoclean and autoremove\n -r to reboot\n -all to do everything\n";
       }
+      else {
+        cout << "You are not using a good parameter, run this with -h argument to get informations on all possibilities\n";
     }
   }
   else {
